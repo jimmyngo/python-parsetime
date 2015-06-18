038c0ce144527e8c65978c9c3bc1c3f28725c371
Jimmy Ngo <jingo@linkedin.com>
2015-06-17 19:36:01 -0700

    Bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ parsetime = Extension('parsetime',
                       sources=['parsetimemodule.c', 'parsetime.c'])
 
 setup(name='python-parsetime',
-      version='2.0.5',
+      version='2.0.6',
       author='Jimmy Ngo',
       author_email='jingo@linkedin.com',
       url='https://github.com/jimmyngo/python-parsetime',

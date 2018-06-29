7a2e01416940ca8b4591bc80d1027d7570801a81
smcgregor <s.mcgregor@griffith.edu.au>
2018-06-29 10:09:40 +1000

    minor version bump, api compat

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ parsetime = Extension('parsetime',
                       sources=['parsetimemodule.c', 'parsetime.c'])
 
 setup(name='python-parsetime',
-      version='2.0.6',
+      version='2.0.7',
       author='Jimmy Ngo',
       author_email='jingo@linkedin.com',
       url='https://github.com/jimmyngo/python-parsetime',
